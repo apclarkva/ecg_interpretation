c9299981e39b89002e6b81fbb84418b673a8d2c3
Alexander Clark <apclarkva@gmail.com>
2019-10-18 17:17:32 -0400

    move stuff from main.py to Models

--- main.py
+++ main.py
@@ -1,65 +1,48 @@
 from ml_models import Models
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd
+from keras.models import load_model
+from sklearn.decomposition import PCA
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 def main():
-    path_to_signals = 'data/ecg/normal_pickled_ind'
-    model_obj = Models(path_to_signals, t_span=2048, num_channels=12)
+    model_obj = Models(t_span=2048, num_channels=12)
+    num_obs = 40 
 
-    num_obs = 300
-    model_obj.load_from_npy(path_to_signals, num_obs)
- 
+    #Data
+    path_to_afibs = 'data/ecg/afib_pickled_ind'
+    path_to_normals = 'data/ecg/normal_pickled_ind'
+    model_obj.load_data(path_to_normals, num_obs, rhythm_type='normal')
+    model_obj.load_data(path_to_afibs, num_files=300, rhythm_type='afib')
+
+    #Setup
     model_obj.get_small_autoencoder()
     model_obj.current_model.summary()
-
-    autoencoder = model_obj.current_model
         
-    history = autoencoder.fit(model_obj.input_data, model_obj.input_data, validation_split=.25, verbose=1, batch_size=30, epochs=80)
-    plot_history(history)
-    model_obj.load_from_npy(path_to_signals, (num_obs + 10))
-    x_test = model_obj.input_data[num_obs:(num_obs+9), :, :]
-    print(autoencoder.evaluate(x_test, x_test))
-    x_predict = autoencoder.predict(x_test)
+    #Run
+    model_obj.run_autoencoder(n_epochs=10)
 
-    plt.plot(x_test[4,:,8])
-    plt.plot(x_predict[4,:,8])
-    plt.show()
+    #Calculate performance
+    model_obj.get_error_by_input()
+    model_obj.evaluate_model()
+    model_obj.predict_test_data(is_plotted)
 
-    import pdb
-    pdb.set_trace()
+    print(f'Evaluation of normal data is {model_obj.norm_eval}')
+    print(f'Evaluation of afib data is {model_obj.afib_eval}')
 
 
-
-
-
-
-
-
-
-def plot_against(autoencoder, x_values):
-    x_predict = autoencoder.predict(x_values)
-
-    plt.plot(x_values[0,:,1])
-    plt.plot(x_predict[0,:,1])
-    plt.show()
-
-
-def plot_history(history):
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-
-
-
+    #encoder stuff
+    model_obj.get_encoder()
+    model_obj.encode_data()
+    model_obj.get_pca_encoded(is_plotted=True)
+    
+    #plotting
+    #model_obj.plot_history()
+    #model_obj.plot_random_test_wave('normal')
+    #model_obj.plot_random_test_wave('afib')
 
 if __name__ == '__main__':
     main()

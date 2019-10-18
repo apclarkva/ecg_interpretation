d65195fd9334f2e8452923810a34361bbf8feb82
Alexander Clark <apc223@curie.pbtech>
2019-10-18 17:21:03 -0400

    updates

--- submit_autoencoder.sh
+++ submit_autoencoder.sh
@@ -2,10 +2,10 @@
  
 #SBATCH --partition=panda   # cluster-specific
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --job-name=christini-job
+#SBATCH --ntasks=28
+#SBATCH --job-name=autoencoder
 #SBATCH --time=08:00:00   # HH/MM/SS
-#SBATCH --mem=5G
+#SBATCH --mem=28G
 
 source ~/.bashrc
 spack load -r /cypfv4n miniconda3@4.3.14%gcc@6.3.0 arch=linux-centos7-x86_64
